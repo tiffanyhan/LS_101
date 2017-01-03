c2869f23731751f028c7c9988d78ce689f544fcd
Tiffany Han <hantiffany@gmail.com>
2017-01-02 21:49:39 -0800

    refactor - write a generic place_piece! method and alternate_player method.  also validate user input.

--- lesson4/tictactoe.rb
+++ lesson4/tictactoe.rb
@@ -12,11 +12,12 @@ MAX_SCORE = 5
 
 INITIAL_MOVERS = ['Player', 'Computer', 'choose']
 
+GO_FIRST_PROMPT = 'Who gets to go first?  Press p for player, c for computer.'
 END_GAME_PROMPT = 'Five games to win a round.  Keep playing?'\
-                  ' (y to continue the round)'
-
+                  ' (y to continue the round, n to leave the round)'
 END_ROUND_PROMPT = 'Would you like to start a new round?' \
-                   ' (y to start a new round)'
+                   ' (y to start a new round, n to exit the program)'
+INVALID_ANS_PROMPT = 'Sorry, that\'s not a valid answer'
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -24,13 +25,13 @@ end
 
 def joinor(arr, conn=', ', word='or')
   case arr.length
-    when 0 then ''
-    when 1 then arr.first
-    when 2 then arr.join(" #{word} ")
-    else
-      arr[-1] = "#{word} #{arr.last}"
-      arr.join(conn)
-    end
+  when 0 then ''
+  when 1 then arr.first
+  when 2 then arr.join(" #{word} ")
+  else
+    arr[-1] = "#{word} #{arr.last}"
+    arr.join(conn)
+  end
 end
 
 # rubocop: disable Metrics/AbcSize
@@ -64,6 +65,14 @@ def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
+def alternate_player(current_player)
+  if current_player == 'Player'
+    'Computer'
+  elsif current_player == 'Computer'
+    'Player'
+  end
+end
+
 def player_places_piece!(brd)
   square = ''
   loop do
@@ -77,10 +86,9 @@ end
 
 def find_winning_square(brd, line, marker)
   if brd.values_at(*line).count(marker) == 2
-    brd.select{|k,v| line.include?(k) && v == INITIAL_MARKER}.keys.first
-  else
-    nil
+    brd.select { |k, v| line.include?(k) && v == INITIAL_MARKER }.keys.first
   end
+  nil
 end
 
 def computer_places_piece!(brd)
@@ -111,6 +119,14 @@ def computer_places_piece!(brd)
   end
 
   brd[square] = COMPUTER_MARKER
+end
+
+def place_piece!(board, current_player)
+  if current_player == 'Player'
+    player_places_piece!(board)
+  elsif current_player == 'Computer'
+    computer_places_piece!(board)
+  end
 end
 
 def board_full?(brd)
@@ -148,68 +164,65 @@ prompt "Welcome to TicTacToe!"
 
 loop do
   # start a new round
-  score = {"Computer" => 0, "Player" => 0}
+  score = { "Computer" => 0, "Player" => 0 }
 
   while score.values.max < MAX_SCORE
     # new game in same round
     board = initialize_board
-    initial_mover = INITIAL_MOVERS.sample
+    initial_mover = INITIAL_MOVERS.last
 
+    # ask user to choose initial mover
     if initial_mover == 'choose'
       loop do
-        prompt "Who gets to go first?  Press p for player, c for computer."
+        prompt GO_FIRST_PROMPT
         answer = gets.chomp.downcase
 
-        h = {'p' => 'Player', 'c' => 'Computer'}
-
-        if h.keys.include?(answer)
-          initial_mover = h[answer]
-          break
-        else
-          prompt "Sorry, that's not a valid choice"
-        end
+        valid_answers = { 'p' => 'Player', 'c' => 'Computer' }
+        initial_mover = valid_answers[answer]
+        break if initial_mover
+        prompt INVALID_ANS_PROMPT
       end
     end
 
+    current_player = initial_mover
     loop do
-      # start turns
-      case initial_mover
-      when 'Computer'
-        computer_places_piece!(board)
-        break if someone_won?(board) || board_full?(board)
-
-        display_board(board, score)
-
-        player_places_piece!(board)
-        break if someone_won?(board) || board_full?(board)
-      when 'Player'
-        display_board(board, score)
-
-        player_places_piece!(board)
-        break if someone_won?(board) || board_full?(board)
-
-        computer_places_piece!(board)
-        break if someone_won?(board) || board_full?(board)
-      end
+      # do a turn
+      display_board(board, score)
+      place_piece!(board, current_player)
+      current_player = alternate_player(current_player)
+      break if someone_won?(board) || board_full?(board)
     end
 
     # show last game board
     score[detect_winner(board)] += 1 if someone_won?(board)
     display_board(board, score)
 
+    # show game results
     show_result(board, score)
 
     # now what?
     if score.values.max < MAX_SCORE
-      prompt END_GAME_PROMPT
-      answer = gets.chomp
-      break unless answer.downcase.start_with?('y')
+      answer = ''
+      loop do
+        prompt END_GAME_PROMPT
+        answer = gets.chomp.downcase
+        break if ['y', 'n'].include?(answer)
+        prompt INVALID_ANS_PROMPT
+      end
+
+      break if answer.start_with?('n')
     end
   end
 
-  prompt END_ROUND_PROMPT
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  answer = ''
+  loop do
+    prompt END_ROUND_PROMPT
+    answer = gets.chomp.downcase
+    break if ['y', 'n'].include?(answer)
+    prompt INVALID_ANS_PROMPT
+  end
+
+  break if answer.start_with?('n')
 end
 
 prompt "Thanks for playing Tic Tac Toe!  Goodbye!"
